2b592deca61d07e0b248c92d35a210fa2c36264d
nickiriber <nickiriber>
2021-10-04 14:07:59 +0000

    added class Position

--- position.py
+++ position.py
@@ -1,3 +1,34 @@
 import numpy as np
 import os
 from datetime import datetime, timedelta
+
+class Position:
+    """A Class for handling Position Data"""
+    #list to hold time, lat, long, heights
+    times = []
+    latitudes = []
+    longitudes = []
+    heights = []
+    
+    data_path = ""
+    
+    #numpy array
+    proj_pos = np.array([])
+    
+    #dict with metadata
+    metadata = dict('geodetic_units': 'rad', #
+               'height_units': 'm',
+               ' proj_units':'m',
+               'geoid_name': None,
+               'ellipsoid_name': None,
+               'height_relative_to': None,
+               'time_basis': 'UTC',
+               'proj_str': 'Unknown')
+    
+
+    def __init__(self):
+        pass
+    
+    def __str__(self): 
+        return self
+    
